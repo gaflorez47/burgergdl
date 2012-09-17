ff847200fe2c346800f395c4d4d14f7014914294
German Florez <german@ooyala.com>
2012-09-17 18:52:57 -0500

    Add full url

--- sites/all/modules/burgergdl/pages/node--restaurant.tpl.php
+++ sites/all/modules/burgergdl/pages/node--restaurant.tpl.php
@@ -1,3 +1,6 @@
+<?php
+$complete_url = url($node_url, array('absolute'=>true));
+?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <?php require "restaurant-$view_mode.tpl.php";?>
 </div>
--- sites/all/modules/burgergdl/pages/social_widgets.tpl.php
+++ sites/all/modules/burgergdl/pages/social_widgets.tpl.php
@@ -1,4 +1,4 @@
-<div class="fb-like" data-href="<?=$node_url?>" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false"></div>
+<div class="fb-like" data-href="<?=$complete_url?>" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false"></div>
 <a href="https://twitter.com/share" class="twitter-share-button" data-lang="en">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 <a href="https://foursquare.com/intent/venue.html" class="fourSq-widget" >Save to foursquare</a>
